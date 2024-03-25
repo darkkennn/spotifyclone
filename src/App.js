d14b09cb6317273ffac534c6e3b5151b4fd4040d
darkkennn <sacpan2000@gmail.com>
2024-03-25 10:59:56 +0530

    extending app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Login from "./Login";
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
 import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState('');
+  const [{user}, dispatch] = useDataLayerValue();
 
-  // run code based on a given condition
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = '';
@@ -17,13 +18,20 @@ function App() {
 
     if (_token) {
       setToken(_token);
-
+      
       spotify.setAccessToken(_token);
 
       spotify.getMe().then(user => {
-        console.log('this', user)
+        console.log('this', user);
+
+        dispatch({
+          type: 'SET_USER',
+          user
+        })
       })
     }}, [])
+
+    console.log(user)
 
   return (
     <div className="App">

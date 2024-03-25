85a956f62f977383a86ffd3a6cdca44260fa3956
darkkennn <sacpan2000@gmail.com>
2024-03-25 11:21:21 +0530

    updating token

--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,7 @@ export const initialState = {
     playlist: [],
     playing: false,
     item: null,
+    //REMOVE THIS AFTER FINISHED DEVELOPING (BY DEFAULT AUTHENTICATED USING THE TOKEN)
     token: 
     "BQDoJ_CImVFsFwmlq1fo5rDSkL0Vu931lmYjg8uE-VXGFp2ZJ9GXNk-MFTfY-omRMj6YL52ORcKlhlA9MpB0YqEZ0wzDkShP8shKhjh8v1zUzZttDjxyVR5s12rudwQY62jJvtu6tlYK36Y78hqNubqMfmtJWefW4AdOd1U4iMuSO3jrZavlhv9wcHvXMp3nXhwx35fY0OH8vcOKzB59",
 }
